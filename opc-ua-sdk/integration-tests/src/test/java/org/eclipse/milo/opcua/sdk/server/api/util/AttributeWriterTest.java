42f1242c2d3427a435a81bb2ede0c8d78e3f0f47
Kevin Herron <kevinherron@gmail.com>
2026-07-01 08:04:14 -0700

    Use DataTypeTree for write type checks (#1782)

--- opc-ua-sdk/integration-tests/src/test/java/org/eclipse/milo/opcua/sdk/server/api/util/AttributeWriterTest.java
+++ opc-ua-sdk/integration-tests/src/test/java/org/eclipse/milo/opcua/sdk/server/api/util/AttributeWriterTest.java
@@ -10,15 +10,30 @@
 
 package org.eclipse.milo.opcua.sdk.server.api.util;
 
+import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.ubyte;
+import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.uint;
+import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.ulong;
+import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.ushort;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.eclipse.milo.opcua.sdk.core.AccessLevel;
+import org.eclipse.milo.opcua.sdk.core.Reference;
+import org.eclipse.milo.opcua.sdk.core.ValueRanks;
 import org.eclipse.milo.opcua.sdk.core.WriteMask;
+import org.eclipse.milo.opcua.sdk.server.AccessContext;
+import org.eclipse.milo.opcua.sdk.server.AttributeWriter;
+import org.eclipse.milo.opcua.sdk.server.nodes.UaDataTypeNode;
+import org.eclipse.milo.opcua.sdk.server.nodes.UaServerNode;
 import org.eclipse.milo.opcua.sdk.server.nodes.UaVariableNode;
 import org.eclipse.milo.opcua.sdk.server.nodes.UaVariableTypeNode;
 import org.eclipse.milo.opcua.sdk.test.AbstractClientServerTest;
+import org.eclipse.milo.opcua.stack.core.AttributeId;
 import org.eclipse.milo.opcua.stack.core.NodeIds;
+import org.eclipse.milo.opcua.stack.core.OpcUaDataType;
 import org.eclipse.milo.opcua.stack.core.StatusCodes;
 import org.eclipse.milo.opcua.stack.core.types.builtin.ByteString;
 import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
@@ -78,6 +93,203 @@ public class AttributeWriterTest extends AbstractClientServerTest {
                 List.of(new NodeId(2, "UByteArray")),
                 List.of(DataValue.valueOnly(new Variant(ByteString.of(new byte[] {1, 2, 3})))))
             .get(0);
+
+    assertEquals(StatusCode.GOOD, statusCode);
+  }
+
+  @Test
+  void writeUnsignedIntegerSubtypesToAbstractUInteger() throws Exception {
+    for (Object unsignedValue : List.of(ubyte(1), ushort(2), uint(3), ulong(4))) {
+      StatusCode statusCode =
+          client
+              .writeValues(
+                  List.of(new NodeId(2, "AbstractUInteger")),
+                  List.of(DataValue.valueOnly(new Variant(unsignedValue))))
+              .get(0);
+
+      assertEquals(StatusCode.GOOD, statusCode);
+    }
+  }
+
+  @Test
+  void rejectSignedIntegerSubtypesForAbstractUInteger() throws Exception {
+    for (Object signedValue : List.of((byte) 1, (short) 2, 3, 4L)) {
+      StatusCode statusCode =
+          client
+              .writeValues(
+                  List.of(new NodeId(2, "AbstractUInteger")),
+                  List.of(DataValue.valueOnly(new Variant(signedValue))))
+              .get(0);
+
+      assertEquals(new StatusCode(StatusCodes.Bad_TypeMismatch), statusCode);
+    }
+  }
+
+  @Test
+  void writeSignedIntegerSubtypesToAbstractInteger() throws Exception {
+    for (Object signedValue : List.of((byte) 1, (short) 2, 3, 4L)) {
+      StatusCode statusCode =
+          client
+              .writeValues(
+                  List.of(new NodeId(2, "AbstractInteger")),
+                  List.of(DataValue.valueOnly(new Variant(signedValue))))
+              .get(0);
+
+      assertEquals(StatusCode.GOOD, statusCode);
+    }
+  }
+
+  @Test
+  void rejectUnsignedIntegerSubtypesForAbstractInteger() throws Exception {
+    for (Object unsignedValue : List.of(ubyte(1), ushort(2), uint(3), ulong(4))) {
+      StatusCode statusCode =
+          client
+              .writeValues(
+                  List.of(new NodeId(2, "AbstractInteger")),
+                  List.of(DataValue.valueOnly(new Variant(unsignedValue))))
+              .get(0);
+
+      assertEquals(new StatusCode(StatusCodes.Bad_TypeMismatch), statusCode);
+    }
+  }
+
+  @Test
+  void writeNumericSubtypesToAbstractNumber() {
+    UaServerNode node = getManagedNode("AbstractNumber");
+
+    for (Object number :
+        List.of(1.0f, 2.0d, (byte) 3, (short) 4, 5, 6L, ubyte(7), ushort(8), uint(9), ulong(10))) {
+      StatusCode statusCode =
+          AttributeWriter.writeAttribute(
+              AccessContext.INTERNAL,
+              node,
+              AttributeId.Value,
+              DataValue.valueOnly(new Variant(number)),
+              null);
+
+      assertEquals(StatusCode.GOOD, statusCode);
+    }
+  }
+
+  @Test
+  void rejectNonBuiltinNumberSubtypesForAbstractNumber() {
+    UaServerNode node = getManagedNode("AbstractNumber");
+
+    for (Number number : List.of(BigInteger.ONE, BigDecimal.ONE, new AtomicInteger(1))) {
+      StatusCode statusCode =
+          AttributeWriter.writeAttribute(
+              AccessContext.INTERNAL,
+              node,
+              AttributeId.Value,
+              DataValue.valueOnly(new Variant(number)),
+              null);
+
+      assertEquals(new StatusCode(StatusCodes.Bad_TypeMismatch), statusCode);
+    }
+  }
+
+  @Test
+  void writeBuiltinValueToBaseDataType() {
+    UaServerNode node = getManagedNode("BaseDataType");
+
+    StatusCode statusCode =
+        AttributeWriter.writeAttribute(
+            AccessContext.INTERNAL,
+            node,
+            AttributeId.Value,
+            DataValue.valueOnly(new Variant("anything")),
+            null);
+
+    assertEquals(StatusCode.GOOD, statusCode);
+  }
+
+  @Test
+  void writeAnythingToBaseDataType() {
+    UaServerNode node = getManagedNode("VariantDataType");
+
+    StatusCode statusCode =
+        AttributeWriter.writeAttribute(
+            AccessContext.INTERNAL,
+            node,
+            AttributeId.Value,
+            DataValue.valueOnly(new Variant(BigDecimal.ONE)),
+            null);
+
+    assertEquals(StatusCode.GOOD, statusCode);
+  }
+
+  @Test
+  void writeIntegerToEnumerationSubtype() throws Exception {
+    StatusCode statusCode =
+        client
+            .writeValues(
+                List.of(new NodeId(2, "ApplicationTypeEnum")),
+                List.of(DataValue.valueOnly(new Variant(1))))
+            .get(0);
+
+    assertEquals(StatusCode.GOOD, statusCode);
+  }
+
+  @Test
+  void rejectNonIntegerForEnumerationSubtype() throws Exception {
+    StatusCode statusCode =
+        client
+            .writeValues(
+                List.of(new NodeId(2, "ApplicationTypeEnum")),
+                List.of(DataValue.valueOnly(new Variant("not an enum"))))
+            .get(0);
+
+    assertEquals(new StatusCode(StatusCodes.Bad_TypeMismatch), statusCode);
+  }
+
+  @Test
+  void writeCustomDataTypeAddedAfterDataTypeTreeIsBuilt() {
+    server.getDataTypeTree();
+
+    NodeId dataTypeId = new NodeId(2, "LateUInt32Subtype");
+
+    testNamespace.configure(
+        (context, nodeManager) -> {
+          var dataTypeNode =
+              new UaDataTypeNode(
+                  context,
+                  dataTypeId,
+                  new QualifiedName(2, "LateUInt32Subtype"),
+                  LocalizedText.english("LateUInt32Subtype"),
+                  LocalizedText.NULL_VALUE,
+                  uint(0),
+                  uint(0),
+                  false);
+
+          dataTypeNode.addReference(
+              new Reference(
+                  dataTypeId,
+                  NodeIds.HasSubtype,
+                  NodeIds.UInt32.expanded(),
+                  Reference.Direction.INVERSE));
+
+          nodeManager.addNode(dataTypeNode);
+
+          UaVariableNode.build(
+              context,
+              b -> {
+                b.setNodeId(new NodeId(2, "LateUInt32SubtypeValue"));
+                b.setBrowseName(new QualifiedName(2, "LateUInt32SubtypeValue"));
+                b.setDisplayName(LocalizedText.english("LateUInt32SubtypeValue"));
+                b.setDataType(dataTypeId);
+                b.setAccessLevel(AccessLevel.READ_WRITE);
+                b.setUserAccessLevel(AccessLevel.READ_WRITE);
+                return b.buildAndAdd();
+              });
+        });
+
+    StatusCode statusCode =
+        AttributeWriter.writeAttribute(
+            AccessContext.INTERNAL,
+            getManagedNode("LateUInt32SubtypeValue"),
+            AttributeId.Value,
+            DataValue.valueOnly(new Variant(uint(42))),
+            null);
 
     assertEquals(StatusCode.GOOD, statusCode);
   }
@@ -173,6 +385,7 @@ public class AttributeWriterTest extends AbstractClientServerTest {
                 b.setBrowseName(new QualifiedName(2, "UByteArray"));
                 b.setDisplayName(LocalizedText.english("UByteArray"));
                 b.setDataType(NodeIds.Byte);
+                b.setValueRank(ValueRanks.OneDimension);
                 b.setArrayDimensions(new UInteger[] {UInteger.valueOf(0)});
                 b.setAccessLevel(AccessLevel.READ_WRITE);
                 b.setUserAccessLevel(AccessLevel.READ_WRITE);
@@ -194,12 +407,84 @@ public class AttributeWriterTest extends AbstractClientServerTest {
           UaVariableNode.build(
               context,
               b -> {
+                b.setNodeId(new NodeId(2, "AbstractNumber"));
+                b.setBrowseName(new QualifiedName(2, "AbstractNumber"));
+                b.setDisplayName(LocalizedText.english("AbstractNumber"));
+                b.setDataType(NodeIds.Number);
+                b.setAccessLevel(AccessLevel.READ_WRITE);
+                b.setUserAccessLevel(AccessLevel.READ_WRITE);
+                return b.buildAndAdd();
+              });
+
+          UaVariableNode.build(
+              context,
+              b -> {
+                b.setNodeId(new NodeId(2, "AbstractUInteger"));
+                b.setBrowseName(new QualifiedName(2, "AbstractUInteger"));
+                b.setDisplayName(LocalizedText.english("AbstractUInteger"));
+                b.setDataType(NodeIds.UInteger);
+                b.setAccessLevel(AccessLevel.READ_WRITE);
+                b.setUserAccessLevel(AccessLevel.READ_WRITE);
+                return b.buildAndAdd();
+              });
+
+          UaVariableNode.build(
+              context,
+              b -> {
+                b.setNodeId(new NodeId(2, "AbstractInteger"));
+                b.setBrowseName(new QualifiedName(2, "AbstractInteger"));
+                b.setDisplayName(LocalizedText.english("AbstractInteger"));
+                b.setDataType(NodeIds.Integer);
+                b.setAccessLevel(AccessLevel.READ_WRITE);
+                b.setUserAccessLevel(AccessLevel.READ_WRITE);
+                return b.buildAndAdd();
+              });
+
+          UaVariableNode.build(
+              context,
+              b -> {
                 b.setNodeId(new NodeId(2, "ReadOnlyVariable"));
                 b.setBrowseName(new QualifiedName(2, "ReadOnlyVariable"));
                 b.setDisplayName(LocalizedText.english("ReadOnlyVariable"));
                 b.setDataType(NodeIds.Int32);
                 b.setAccessLevel(AccessLevel.READ_ONLY);
                 // allow the service call to reach AttributeWriter
+                b.setUserAccessLevel(AccessLevel.READ_WRITE);
+                return b.buildAndAdd();
+              });
+
+          UaVariableNode.build(
+              context,
+              b -> {
+                b.setNodeId(new NodeId(2, "ApplicationTypeEnum"));
+                b.setBrowseName(new QualifiedName(2, "ApplicationTypeEnum"));
+                b.setDisplayName(LocalizedText.english("ApplicationTypeEnum"));
+                b.setDataType(NodeIds.ApplicationType);
+                b.setAccessLevel(AccessLevel.READ_WRITE);
+                b.setUserAccessLevel(AccessLevel.READ_WRITE);
+                return b.buildAndAdd();
+              });
+
+          UaVariableNode.build(
+              context,
+              b -> {
+                b.setNodeId(new NodeId(2, "BaseDataType"));
+                b.setBrowseName(new QualifiedName(2, "BaseDataType"));
+                b.setDisplayName(LocalizedText.english("BaseDataType"));
+                b.setDataType(NodeIds.BaseDataType);
+                b.setAccessLevel(AccessLevel.READ_WRITE);
+                b.setUserAccessLevel(AccessLevel.READ_WRITE);
+                return b.buildAndAdd();
+              });
+
+          UaVariableNode.build(
+              context,
+              b -> {
+                b.setNodeId(new NodeId(2, "VariantDataType"));
+                b.setBrowseName(new QualifiedName(2, "VariantDataType"));
+                b.setDisplayName(LocalizedText.english("VariantDataType"));
+                b.setDataType(OpcUaDataType.Variant.getNodeId());
+                b.setAccessLevel(AccessLevel.READ_WRITE);
                 b.setUserAccessLevel(AccessLevel.READ_WRITE);
                 return b.buildAndAdd();
               });
@@ -228,5 +513,10 @@ public class AttributeWriterTest extends AbstractClientServerTest {
                 return b.buildAndAdd();
               });
         });
+  }
+
+  private UaServerNode getManagedNode(String identifier) {
+    return (UaServerNode)
+        server.getAddressSpaceManager().getManagedNode(new NodeId(2, identifier)).orElseThrow();
   }
 }
